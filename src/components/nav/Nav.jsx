e17b5cd53ad8bc6903b8c3177d4ca7a69f8cc69f
KAUSHAN JAYASUNDARA <57662753+nipunakaushan@users.noreply.github.com>
2023-06-24 04:18:30 +0530

    Floating Nav Component jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceFill} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><BiBook /></a>
+      <a href='#services'><RiServiceFill /></a>
+      <a href='#contact'><BiMessageSquareDetail /></a>
+      
+    </nav>
+    
+    
   )
 }
 
