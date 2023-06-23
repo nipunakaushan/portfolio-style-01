b05a3e4bd3da7bd2c758986731c0b1cd0a3afade
KAUSHAN JAYASUNDARA <57662753+nipunakaushan@users.noreply.github.com>
2023-06-24 01:53:56 +0530

    custermize 50% header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+
 
 const header = () => {
   return (
     <header>
-      <div className="container.header__container">
+      <div className="container header__container">
         <h5>Hello I'am</h5>
         <h1>Kaushan Jayasundara</h1>
-        <h5 className="text-light">Software Engeneer</h5>
+        <h5 className="text-light">SOFTWARE ENGINEER</h5>
+        <CTA />
+
+        <div className='me'>
+        <img src={ME} alt='me' />
+        </div>  
+
+        <a href="#content" className='scroll__down'>Scroll Down</a>
+
       </div>
     </header>
   )
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/kaushan-jayasundara-01b1441bb' target='_blank'>linkedin</a>
+        <a href='https://github.com/nipunakaushan' target='_blank'>linkedin</a>
+        <a href='https://www.linkedin.com/in/kaushan-jayasundara-01b1441bb' target='_blank'>linkedin</a>
+
+
+    </div>
+  )
+}
+
+export default HeaderSocials
