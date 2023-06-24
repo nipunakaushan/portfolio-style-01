dcb76ca26b208675cb88b39138ff4f9d4a7cb88a
KAUSHAN JAYASUNDARA <57662753+nipunakaushan@users.noreply.github.com>
2023-06-24 22:06:37 +0530

    implement portfolio,testimonials and contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import{RiMessengerLine} from 'react-icons/ri'
+import{BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_zphbqjg', 'template_n1on3qd', form.current, 'x57eGzdjV4ZyPEUn3')
+
+    e.target.reset()
+      
+  };
+
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Het In Touch</h5>
+      <h2>contact</h2>
+      
+      <div className="container contact__container">
+          <div className="contact__options">
+            <article className="contact__option">
+                <MdOutlineMail className='contact__option-icon'/>
+                <h4>Email</h4>
+                <h5>kaushanjayasundara.lankanvibes.com</h5>
+                <a href="mailto:kaushanjayasundara@lankanvibes.com" target='_blank'>Send a message</a>
+            </article>
+
+            <article className="contact__option">
+                <BsWhatsapp className='contact__option-icon'/>
+                <h4>Whatsapp</h4>
+                <h5>+94775500156</h5>
+                <a href="http://api.whatsapp.com/send?phone=+94775500156" target='_blank'>Send a message</a>
+            </article>
+
+            <article className="contact__option">
+                <RiMessengerLine className='contact__option-icon'/>
+                <h4>Messenger</h4>
+                <h5>kaushanjayasundara</h5>
+                <a href="http://m.me/profile.php?id=100090921799259" target='_blank'>Send a message</a>
+            </article>
+
+          </div>
+          {/*EMD OF CONTACT OPTION */}
+          <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+
+          </form>
+        
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,98 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+// do not use images in production
+const data = [
+  {
+    id:1,
+    image:IMG1,
+    title:'Online Ticketing Platform',
+    github: 'https://github.com' ,
+    demo: 'https://onlineticket.lk/'
+
+  },
+
+  {
+    id:2,
+    image:IMG2,
+    title:'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com' ,
+    demo: 'https://onlineticket.lk/'
+
+  },
+
+  {
+    id:3,
+    image:IMG3,
+    title:'Figma dashboard UI Kit for Data design web apps ',
+    github: 'https://github.com' ,
+    demo: 'https://onlineticket.lk/'
+
+  },
+
+  {
+    id:4,
+    image:IMG4,
+    title:'Maintaining tasks and tracking progress',
+    github: 'https://github.com' ,
+    demo: 'https://onlineticket.lk/'
+
+  },
+
+  {
+    id:5,
+    image:IMG5,
+    title:'Charts template & infographics in Figma',
+    github: 'https://github.com' ,
+    demo: 'https://onlineticket.lk/'
+
+  },
+
+  {
+    id:6,
+    image:IMG6,
+    title:'Android Mobile Application',
+    github: 'https://github.com' ,
+    demo: 'https://onlineticket.lk/'
+
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio/Projects</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+            <article key={id} className='portfolio__item'>
+
+            <div className="portfolio__item-image">
+            <img src={image} alt='title' />
+            </div>
+
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn' target='_blank'>github</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
+        </article>
+            )
+          })
+        }
+        
+      </div>    
+      
+      </section>
   )
 }
 
